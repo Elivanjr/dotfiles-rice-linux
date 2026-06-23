31bc5c4765040b6eda2093149b65a5c6e881b664
Elivan Junior <elivanvljr@gmail.com>
2026-06-22 23:52:57 -0300

    Exibe artista e álbum no tooltip do Spotify quando o mouse descansa sob o modulo...

--- .config/waybar/scripts/spotify.sh
+++ .config/waybar/scripts/spotify.sh
@@ -1,23 +1,33 @@
 #!/usr/bin/bash
 
-class=$(playerctl metadata --player=spotify --format '{{lc(status)}}')
-icon=" "
+class=$(playerctl metadata --player=spotify --format '{{lc(status)}}' 2>/dev/null)
+icon=""
 
 if [[ $class == "playing" ]]; then
-  info=$(playerctl metadata --player=spotify --format '{{artist}} - {{title}}')
+  artist=$(playerctl metadata --player=spotify artist)
+  title=$(playerctl metadata --player=spotify title)
+  album=$(playerctl metadata --player=spotify album)
 
-  info=$(echo "$info" | awk '{print substr($0,1,30)}')
+  info="$artist - $title"
+
+  short=$(echo "$info" | awk '{print substr($0,1,30)}')
   if [[ ${#info} -ge 30 ]]; then
-    info="$info..."
+    short="$short..."
   fi
 
-  text="$icon $info"
+  text="$icon $short"
+
+  #  tooltip=" $title\n $artist\n󰀥 $album"
+  tooltip="<big><b> $title</b></big>\n $artist\n󰀥 $album"
 
 elif [[ $class == "paused" ]]; then
   text="$icon"
+  tooltip="<big><b>󰏤 Spotify pausado</b></big>"
 
 elif [[ $class == "stopped" ]]; then
   text=""
+  tooltip=""
+
 fi
 
-echo "{\"text\":\"$text\", \"class\":\"$class\"}"
+echo "{\"text\":\"$text\", \"tooltip\":\"$tooltip\", \"class\":\"$class\"}"
