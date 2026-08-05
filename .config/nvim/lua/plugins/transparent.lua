a9eeef6d21875ffa041b9da3b78b122e2c412647
Elivanjr <elivanvljr@gmail.com>
2026-08-05 15:15:31 -0300

    Efeito de transparencia...

---
+++ .config/nvim/lua/plugins/transparent.lua
@@ -0,0 +1,16 @@
+return {
+  "tribela/transparent.nvim",
+  lazy = false,
+  priority = 1000,
+  opts = {
+    extra_groups = {
+      "NormalFloat",
+      "FloatBorder",
+      "FloatTitle",
+
+      -- Snacks
+      "SnacksNormal",
+      "SnacksBorder",
+    },
+  },
+}
